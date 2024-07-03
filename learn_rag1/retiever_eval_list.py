b02981e5cf0f8a412745cdc4b63f044577e19699
zlyzly <zlyzly@163.com>
2024-07-03 18:18:34 +0800

    73

--- learn_rag1/retiever_eval_list.py
+++ learn_rag1/retiever_eval_list.py
@@ -43,7 +43,7 @@ def get_ndcg_score(lab_index_list, retriever_list):
 
 def soft_ht_score(lab_index, retriever_res, top_k = 8, so = 3)->float:
     list1 = range(lab_index-so, lab_index+so)
-    if any(element in retriever_res[:top_k] for element in list1):
+    if any(element in retriever_res for element in list1):
         return 1.0
     else:
         return 0.0
@@ -52,20 +52,20 @@ def soft_mmr_score(lab_index, retriever_res, top_k = 8, so = 3)->float:
     list1 = range(lab_index-so, lab_index+so)
     ank = 0
     for element in list1:
-        if element in retriever_res[:top_k]:
+        if element in retriever_res:
             ank += 1.0 / (retriever_res.index(element) + 1)
     return ank
 
 def ht_score(lab_index, retriever_res, top_k = 8, so = 3)->float:
-    if lab_index in retriever_res[:top_k]:
+    if lab_index in retriever_res:
         return 1.0
     else:
         return 0.0
 
 def mmr_score(lab_index, retriever_res, top_k = 8, so = 3)->float:
     ank = 0
-    if lab_index in retriever_res[:top_k]:
-        ank += 1.0 / (retriever_res[:top_k].index(lab_index) + 1)
+    if lab_index in retriever_res:
+        ank += 1.0 / (retriever_res.index(lab_index) + 1)
     return ank
 
 def get_ht_score(lab_index_list, retriever_list):
@@ -100,3 +100,30 @@ def get_retriever_res_list(document_list, top_k = 20):
             result.append(doc.metadata['id'])
         id_tmp.append(result)
     return id_tmp
+
+
+def get_result_retrieva(col_id, r_result):
+    rerank_evaldict = {}
+
+    for i in range(2, 10):
+        # print('top_', i+1)
+        res_retriever_list = get_retriever_res_list(r_result, i+1)
+        # print(res_retriever_list[0])
+        ht_score = get_ht_score(col_id, res_retriever_list)
+        mmr_score = get_mmr_score(col_id, res_retriever_list)
+        # print('ht:', round(ht_score, 3))
+        # print('mmr_score:', round(mmr_score, 3))
+        soft__ht_score = get_soft_ht_score(col_id, res_retriever_list)
+        soft__mmr_score = get_soft_mmr_score(col_id, res_retriever_list)
+        # print('soft__ht_score:', round(soft__ht_score, 3))
+        # print('soft__mmr_score:', round(soft__mmr_score, 3))
+        ndcg_score = get_ndcg_score(col_id, res_retriever_list)
+        # print('ndcg:', round(ndcg_score, 3))
+        rerank_evaldict['top_' + str(i+1)] = {
+            "ht_score": round(ht_score, 3),
+            'mmr_score': round(mmr_score, 3),
+            'soft_ht_score': round(soft__ht_score, 3),
+            'soft_mmr_score': round(soft__mmr_score, 3),
+            'ndcg': round(ndcg_score, 3),
+        }
+    return rerank_evaldict
